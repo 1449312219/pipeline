3a98afd8165b06ec58ff7bea0009c20e082674fa
kongshuai <kongshuai_jr@dhcc.com.cn>
2020-10-07 22:56:53 +0800

    加入部署通知trigger

--- script/build.sh
+++ script/build.sh
@@ -11,5 +11,11 @@ function addNamespace() {
 # branchType env1 env2 env3 (流水线内的环境)
 ./branch-push-build.sh | addNamespace
 
+# webhook
+cat templates/deployed-notify/trigger.yaml | addNamespace
+
+manifestSuffix="-manifest"
+webhook="http://deployed-notify.${projectName}-pipeline:8080"
+
 # branchType manifestSuffix webhook env1 env2 env3 (需创建flux的环境)
 ./flux-init-build.sh | addNamespace
