0118203b3c98f712a5e105df075d2f25c17d56ab
kongshuai <kongshuai_jr@dhcc.com.cn>
2020-10-07 21:55:12 +0800

    加入namespace配置

--- script/build.sh
+++ script/build.sh
@@ -2,8 +2,14 @@
 #./init-build.sh $@
 #./pipeline-build.sh Release- ci
 
+projectName=test
+
+function addNamespace() {
+  sed -e "/^metadata:/a\  namespace: ${projectName}-pipeline" -e "/namespace/d"
+}
+
 # branchType env1 env2 env3 (流水线内的环境)
-./branch-push-build.sh
+./branch-push-build.sh | addNamespace
 
 # branchType manifestSuffix webhook env1 env2 env3 (需创建flux的环境)
-./flux-init-build.sh
+./flux-init-build.sh | addNamespace
