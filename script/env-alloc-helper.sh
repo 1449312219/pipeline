359fad1cb409c6838671576a767e5411b7f1051f
kongshuai <kongshuai_jr@dhcc.com.cn>
2020-10-22 17:00:32 +0800

    遗漏提交

---
+++ script/env-alloc-helper.sh
@@ -0,0 +1,46 @@
+. _help.sh
+
+purpose=$1
+purposeForNs=$(formatToNamespace $purpose false)
+export PURPOSE=$purposeForNs
+shift
+
+branchType=$1
+shift
+
+export MANIFEST_SUFFIX=$1
+shift
+
+export WEBHOOK=$1
+shift
+
+export ENV="$@"
+
+
+# 选取需部署的环境
+ENV_DIR=env
+declare -a envs
+for i in $ENV; do
+  if test "$(cat $ENV_DIR/$i/config.yaml | printYamlContent deploy)" == "true"; then
+    envs+=($i)
+  fi
+done
+export ENV="${envs[@]}"
+
+if test ${#envs[@]} -le 0; then
+  exit
+fi
+
+
+TEMP_DIR=templates/env-alloc
+
+# pipeline
+parsePlaceHolder $TEMP_DIR/pipeline.yaml
+printSplit
+
+# trigger
+parsePlaceHolder $TEMP_DIR/trigger.yaml
+printSplit
+
+# webhook
+addWebHook http://el-${PURPOSE}-branch-created.'${NAMESPACE}':8080 ${branchType} create
