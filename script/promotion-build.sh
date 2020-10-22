eb438e09eaa16af17e25a9daa948717a71cd4ab7
kongshuai <kongshuai_jr@dhcc.com.cn>
2020-10-22 09:52:38 +0800

    promotion调整 trigger name

--- script/promotion-build.sh
+++ script/promotion-build.sh
@@ -50,7 +50,7 @@ function branchPushPromotion() {
   parsePlaceHolder $TEMP_DIR/branch-push-trigger.yaml
   printSplit
 
-  addWebHook http://${purpose}branch-push.'${NAMESPACE}':8080 ${branchType} push
+  addWebHook http://${PURPOSE}-branch-push.'${NAMESPACE}':8080 ${branchType} push
 }
 
 
