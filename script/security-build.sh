4537f8dc923b3ca759454c6f306f86aaffa7f157
kongshuai <kongshuai_jr@dhcc.com.cn>
2020-10-26 17:42:08 +0800

    将robot-keys改成ssh-auth格式

--- script/security-build.sh
+++ script/security-build.sh
@@ -6,15 +6,12 @@ shift
 export NAMESPACE=$1
 shift
 
-export GIT_SERVER_SSH=$(kubectl get configmap owner-config -o jsonpath={.data.git-server-ssh})
+robotKeys=($(kubectl get secret robot-keys --no-headers -o custom-columns=token:data.token,git:.metadata.annotations.tekton\\.dev/git-0,hosts:data.known_hosts,key:data.ssh-privatekey))
 
-export KNOWN_HOSTS=$(echo $GIT_SERVER_SSH \
-                     | sed -r '/:[0-9]+/ s|([^:]+):([^:]+)|ssh-keyscan -p \2 \1 2>/dev/null|e' \
-                     | sed -r 's/.*/"&"/' )
-
-robotKeys=($(kubectl get secret robot-keys --no-headers -o custom-columns=token:data.token,key:data.ssh-private-key))
 export GITEA_USER_TOKEN=${robotKeys[0]}
-export SSH_PRIVATE_KEY=${robotKeys[1]}
+export GIT_SERVER_SSH=${robotKeys[1]}
+export KNOWN_HOSTS=${robotKeys[2]}
+export SSH_PRIVATE_KEY=${robotKeys[3]}
 
 
 TEMP_DIR=templates/security
