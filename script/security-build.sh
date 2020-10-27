4f4d09449f36b5c23339f76d7847ab59aeeddd59
kongshuai <kongshuai_jr@dhcc.com.cn>
2020-10-27 09:45:20 +0800

    security-build 可解析不含端口的ssh地址

--- script/security-build.sh
+++ script/security-build.sh
@@ -9,8 +9,9 @@ shift
 export GIT_SERVER_SSH=$(kubectl get configmap owner-config -o jsonpath={.data.git-server-ssh})
 
 export KNOWN_HOSTS=$(echo $GIT_SERVER_SSH \
-                     | sed -r '/:[0-9]+/ s|([^:]+):([^:]+)|ssh-keyscan -p \2 \1 2>/dev/null|e' \
-                     | sed -r 's/.*/"&"/' )
+                     | sed -r '/:[0-9]+/ s|([^:]+):([^:]+)|-p \2 \1|' \
+                     | xargs ssh-keyscan 2>/dev/null \
+                     | sed -r 's/.*/"&"/')
 
 robotKeys=($(kubectl get secret robot-keys --no-headers -o custom-columns=token:data.token,key:data.ssh-private-key))
 export GITEA_USER_TOKEN=${robotKeys[0]}
