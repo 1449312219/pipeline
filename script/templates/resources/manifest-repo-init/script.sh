fe1e5268a1ef79d78503a9014afba8c207fa87c4
kongshuai <kongshuai_jr@dhcc.com.cn>
2020-11-27 21:54:23 +0800

    移除初版无用内容

--- script/templates/resources/manifest-repo-init/script.sh
+++
@@ -1,5 +0,0 @@
-repoPath=$1
-
-mkdir $repoPath/common_resources -p
-
-cp kustomization.yaml $repoPath/common_resources/
--- tasks/configmap/_helper.sh
+++
@@ -1,4 +0,0 @@
-name=$1
-dir=${2:-$1}
-kubectl create configmap $name --from-file=$dir --dry-run=client -o yaml \
-| kubectl apply -f -
