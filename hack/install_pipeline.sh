a6f34cbfea1fd6dd0acca7948049e157446a4b4d
kongshuai <kongshuai_jr@dhcc.com.cn>
2020-12-03 16:51:53 +0800

    加入安装脚本

---
+++ hack/install_pipeline.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# copy form https://github.com/kubernetes-sigs/kustomize/edit/master/hack/install_kustomize.sh
+
+release_url=https://api.github.com/repos/1449312219/pipeline/releases
+
+if [ -n "$1" ]; then
+    version=v$1
+    release_url=${release_url}/tags/$version
+fi
+
+where=$PWD/pipeline
+if [ -e $where ]; then
+  echo "A file named pipeline already exists (remove it first)."
+  exit 1
+fi
+mkdir $where
+
+tmpDir=`mktemp -d`
+if [[ ! "$tmpDir" || ! -d "$tmpDir" ]]; then
+  echo "Could not create temp dir."
+  exit 1
+fi
+
+function cleanup {
+  rm -rf "$tmpDir"
+}
+
+trap cleanup EXIT
+
+pushd $tmpDir >& /dev/null
+
+curl -s $release_url |\
+  grep tarball_url |\
+  cut -d '"' -f 4 |\
+  sort | tail -n 1 |\
+  xargs curl -sLo pipeline.tar.gz
+
+if [ -e ./pipeline.tar.gz ]; then
+    tar xzf ./pipeline.tar.gz
+else
+    echo "Error: pipeline package does not exist!"
+    exit 1
+fi
+
+cp ./*/* $where -R
+
+popd >& /dev/null
+
+chmod u+x $where/install.sh
+
+echo pipeline installed to current directory.
